af2ccc95ab0c74245120ad08c287cb257c2faccf
Riyad <Riyad9876@gmail.com>
2023-07-17 09:36:59 -0400

    game functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+console.log(`Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship.Battle the aliens as you try to destroy them with your lasers.There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.`);
+
+
+
 const attackBtn = document.querySelector('#attack-btn');
 
 
@@ -10,7 +14,17 @@ class Ship {
 
     attack(ship) {//pass in object instance whose health to reduce
         console.log(`${this.shipName} is attacking.... ${ship.shipName}`);
-        console.dir(this);
+        console.log(`${ship.shipName}'s hull is ${ship.hull}. ${this.shipName} attacks it for ${this.firepower}`);
+        ship.hull -= this.firepower
+        console.log(`${ship.shipName}'s hull is now ${ship.hull}`);
+        if (ship.hull <= 0){
+            console.log(`You have slain ${ship.shipName}!`);
+            return true //returns true if ship is killed, will then use value to either end game if user's ship was destroyed, or to take slain enemy ship out of array
+        }
+        else{
+            return false
+        }
+        // console.dir(this);
     }
 }
 
@@ -22,6 +36,11 @@ class userShip extends Ship{
         this.firepower = 5
         
       }
+
+    retreat(enemyShips){
+        console.log(`You have successfully retreated from battle. There were ${enemyShips.length} left.`);
+        gameIsOn = false
+    }
 }
 
 class enemyShip extends Ship{
@@ -51,3 +70,20 @@ console.log(enemyShips);
 
 
 
+// while (gameIsOn){
+//     humanShip.attack(enemyShips[0])
+//     console.log(enemyShips);
+
+// }
+
+if (enemyShips.length > 0){//checks if anymore enemy ships left, keep repeating in game loop
+    console.log(`There are ${enemyShips.length} enemy ships remaining. Would you like to attack the next ship or retreat?`);
+    if (humanShip.attack(enemyShips[0])){
+        enemyShips.shift()
+    }
+    console.log(enemyShips);
+    // if (enemyShips[0].hull <= 0)
+}
+else{
+    console.log(`There are no more enemy ships! You win!`);
+}
